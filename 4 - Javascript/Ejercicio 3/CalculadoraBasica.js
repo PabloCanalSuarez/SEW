dd2c2a27c9f71232d2cff9bcd6dac3cc8f57ead1
Pablo Cañal <pablocsmw3@gmail.com>
2019-11-19 15:22:38 +0100

    Fixed calculadoraBasica

--- 4 - Javascript/Ejercicio 3/CalculadoraBasica.js
+++ 4 - Javascript/Ejercicio 3/CalculadoraBasica.js
@@ -1,48 +1,49 @@
-var memory = 0
+class Calculadora {
 
-//saves into memory the number of the display
-//if memory != 0, adds one to the memory value
-function saveMemPlus()
-{
-    if(memory == 0){
-        memory = eval(document.getElementById("result").value)
-    } else {
-        memory = eval(memory + 1)
+    constructor(){
+        this.memory = 0;
     }
-}
 
-//performs the operation memory-1
-function saveMemMinus()
-{
-    if(memory != 0){
-        memory = eval(memory - 1)
+    //saves into memory the number of the display
+    //if memory != 0, adds one to the memory value
+    saveMemPlus()
+    {
+        this.memory += eval(document.getElementById("result").value)
     }
-}
-
-//displays the memory value
-function showMemory()
-{
-    if(memory != 0){
-        dis(memory)
+    
+    //performs the operation memory-1
+    saveMemMinus()
+    {
+        this.memory -= eval(document.getElementById("result").value)
+    }
+    
+    //displays the memory value
+    showMemory()
+    {
+        if(this.memory != 0){
+            this.dis(this.memory)
+        } 
+    }
+    
+    //display value 
+    dis(val) 
+    { 
+        document.getElementById("result").value+=val
+    } 
+    
+    //evaluates the expression and returns result 
+    solve() 
+    { 
+        let x = document.getElementById("result").value 
+        let y = eval(x) 
+        document.getElementById("result").value = y 
+    } 
+    
+    //clears the display 
+    clr() 
+    { 
+        document.getElementById("result").value = "";
     } 
 }
 
-//display value 
-function dis(val) 
-{ 
-    document.getElementById("result").value+=val
-} 
-
-//evaluates the expression and returns result 
-function solve() 
-{ 
-    let x = document.getElementById("result").value 
-    let y = eval(x) 
-    document.getElementById("result").value = y 
-} 
-
-//clears the display 
-function clr() 
-{ 
-    document.getElementById("result").value = "" 
-} 
+var calculadora = new Calculadora();
--- 4 - Javascript/Ejercicio 4/CalculadoraBasica.js
+++ 4 - Javascript/Ejercicio 4/CalculadoraBasica.js
@@ -1,10 +1,14 @@
+class Calculadora {
+
+    constructor() {
+        this.memory = 0;
+    }
 var memory = 0
 
 //saves into memory the number of the display
 //if memory != 0, adds one to the memory value
-function saveMemPlus()
-{
-    if(memory == 0){
+function saveMemPlus() {
+    if (memory == 0) {
         memory = eval(document.getElementById("result").value)
     } else {
         memory = eval(memory + 1)
@@ -12,75 +16,67 @@ function saveMemPlus()
 }
 
 //performs the operation memory-1
-function saveMemMinus()
-{
-    if(memory != 0){
+function saveMemMinus() {
+    if (memory != 0) {
         memory = eval(memory - 1)
     }
 }
 
 //displays the memory value
-function showMemory()
-{
-    if(memory != 0){
+function showMemory() {
+    if (memory != 0) {
         dis(memory)
-    } 
+    }
 }
 
 //display value, concatenating to the value of display 
-function dis(val) 
-{ 
+function dis(val) {
     document.getElementById("result").value += val
-} 
+}
 
 //evaluates the expression and returns result 
-function solve() 
-{ 
-    let x = document.getElementById("result").value 
-    let y = eval(x) 
-    document.getElementById("result").value = y 
-} 
+function solve() {
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
 
 //clears the display 
-function clr() 
-{ 
-    document.getElementById("result").value = "" 
-} 
+function clr() {
+    document.getElementById("result").value = ""
+}
 
 //display value, erasing any value of dislay 
-function newDisplay(val) 
-{ 
+function newDisplay(val) {
     document.getElementById("result").value = val
-} 
+}
 
-function sin()
-{
+function sin() {
     let x = Math.sin(document.getElementById("result").value)
     newDisplay(x)
 }
 
-function cos()
-{
+function cos() {
     let x = Math.cos(document.getElementById("result").value)
     newDisplay(x)
 }
 
-function tan()
-{
+function tan() {
     let x = Math.tan(document.getElementById("result").value)
     newDisplay(x)
 }
 
-function xp2()
-{
+function xp2() {
     let disp = document.getElementById("result").value
     let x = eval(disp) * eval(disp)
     newDisplay(x)
 }
 
-function xpy()
-{
+function xpy() {
     let disp = document.getElementById("result").value
     let x = eval(disp) * eval(disp)
     newDisplay(x)
 }
+}
+
+var calculadora = new Calculadora();
